818bbdb926dbc825b88db0c588bd4e5b5cbda656
RemixFX <remix.master@yandex.ru>
2023-07-27 20:31:34 +0300

    adding multiple files to a state

---
+++ app/components/FileUpload/file-upload.tsx
@@ -0,0 +1,10 @@
+/* "use client"
+
+import styles from './file-upload.module.css'
+
+export default function FileUpload() {
+  return(
+    <div className={}
+  )
+}
+ */
--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,53 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { ChangeEvent, useState } from 'react'
 
 export default function Home() {
+
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+    const fileList: FileList = e.target.files!
+    const newFilesArray: File[] = Array.from(fileList)
+    if (files) {
+      setFiles(files.concat(newFilesArray))
+    }
+    else {
+      setFiles(newFilesArray)
+    }
+  };
+
+  const handleUploadClick = () => {
+    if (!files) {
+      return;
+    } else {
+
+      const data = new FormData();
+      files.forEach((file) => {
+        data.append(file.name, file);
+      });
+    }
+  }
+
+
+
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <input type="file" onChange={handleFileChange} multiple />
+        <ul>
+          {files.map((file, i) => (
+            <li key={i}>
+              {file.name} - {file.type}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleUploadClick}>Upload</button>
       </div>
     </main>
   )
